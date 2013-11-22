a13b9350f324498cfbe41f5bea174272aa24c55e
Peter Waldschmidt <peterw@gnoso.com>
2013-11-22 18:01:44 -0500

    Add support for enumeration tagging in the schema file.
    Generate a map[EnumType]string that maps the enumeration codes to a friendly string. The string is taken from a $tag annotation added to each declaration in the schema file.

--- go.capnp
+++ go.capnp
@@ -1,4 +1,5 @@
 @0xd12a1c51fedd6c88;
 annotation package(file) :Text;
 annotation import(file) :Text;
+annotation tag(enumerant) : Text;
 $package("capn");
--- go.capnp.go
+++ go.capnp.go
@@ -2,3 +2,4 @@ package capn
 
 const Package = uint64(0xbea97f1023792be0)
 const Import = uint64(0xe130b601260e44b5)
+const Tag = uint64(0xa574b41924caefc7)
