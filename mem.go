fbd1ddff14f1f78484f2808e4416507bcdc71672
glycerine <j.e.aten@gmail.com>
2013-12-18 17:53:22 -0800

    multiBuffer -> MultiBuffer, make MultiBuffer public because otherwise gob could not register/encode a client of go-capnproto

--- mem.go
+++ mem.go
@@ -47,7 +47,7 @@ func (b *buffer) Lookup(segid uint32) (*Segment, error) {
 	}
 }
 
-type multiBuffer struct {
+type MultiBuffer struct {
 	segments []*Segment
 }
 
@@ -55,8 +55,8 @@ type multiBuffer struct {
 // will try and reuse the buffers available, but will create new ones if there
 // is insufficient capacity. When parsing an existing message data should be
 // the list of segments. The data buffers will not be copied.
-func NewmultiBuffer(data [][]byte) *Segment {
-	m := &multiBuffer{make([]*Segment, len(data))}
+func NewMultiBuffer(data [][]byte) *Segment {
+	m := &MultiBuffer{make([]*Segment, len(data))}
 	for i, d := range data {
 		m.segments[i] = &Segment{m, d, uint32(i)}
 	}
@@ -71,7 +71,7 @@ var (
 	MaxTotalSize     = 1024 * 1024 * 1024
 )
 
-func (m *multiBuffer) NewSegment(minsz int) (*Segment, error) {
+func (m *MultiBuffer) NewSegment(minsz int) (*Segment, error) {
 	for _, s := range m.segments {
 		if len(s.Data)+minsz <= cap(s.Data) {
 			return s, nil
@@ -86,7 +86,7 @@ func (m *multiBuffer) NewSegment(minsz int) (*Segment, error) {
 	return s, nil
 }
 
-func (m *multiBuffer) Lookup(segid uint32) (*Segment, error) {
+func (m *MultiBuffer) Lookup(segid uint32) (*Segment, error) {
 	if uint(segid) < uint(len(m.segments)) {
 		return m.segments[segid], nil
 	} else {
@@ -141,7 +141,7 @@ func ReadFromStream(r io.Reader, buf *bytes.Buffer) (*Segment, error) {
 		return NewBuffer(datav[:sz]), nil
 	}
 
-	m := &multiBuffer{make([]*Segment, segnum)}
+	m := &MultiBuffer{make([]*Segment, segnum)}
 	for i := 0; i < segnum; i++ {
 		sz := int(little32(hdrv[4*i:])) * 8
 		m.segments[i] = &Segment{m, datav[:sz], uint32(i)}
@@ -187,7 +187,7 @@ func ReadFromMemoryZeroCopy(data []byte) (seg *Segment, bytesRead int64, err err
 
 	hdrv := data[4:(hdrsz + 4)]
 	datav := data[hdrsz+4:]
-	m := &multiBuffer{make([]*Segment, segnum)}
+	m := &MultiBuffer{make([]*Segment, segnum)}
 	for i := 0; i < segnum; i++ {
 		sz := int(little32(hdrv[4*i:])) * 8
 		m.segments[i] = &Segment{m, datav[:sz], uint32(i)}
