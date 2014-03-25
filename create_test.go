2aecffd2c99cb03de2d501349d71fda7a90a96c1
glycerine <j.e.aten@gmail.com>
2014-03-24 22:04:14 -0700

    add test for packed zdate and for zdata correct creation

--- create_test.go
+++ create_test.go
@@ -41,3 +41,50 @@ func TestCreationOfManyZDate(t *testing.T) {
 		})
 	})
 }
+
+func TestCreationOfManyZDatePacked(t *testing.T) {
+	const n = 10
+	packed := true
+	seg, _ := zdateFilledSegment(n, packed)
+	text := CapnpDecodeSegment(seg, "", "test.capnp", "Z")
+
+	expectedText := `(zdatevec = [(year = 2004, month = 12, day = 7), (year = 2005, month = 12, day = 7), (year = 2006, month = 12, day = 7), (year = 2007, month = 12, day = 7), (year = 2008, month = 12, day = 7), (year = 2009, month = 12, day = 7), (year = 2010, month = 12, day = 7), (year = 2011, month = 12, day = 7), (year = 2012, month = 12, day = 7), (year = 2013, month = 12, day = 7)])`
+
+	cv.Convey("Given a go-capnproto created a PACKED segment with 10 Zdate", t, func() {
+		cv.Convey("When we decode it with capnp", func() {
+			cv.Convey(fmt.Sprintf("Then we should get the expected text '%s'", expectedText), func() {
+				cv.So(text, cv.ShouldEqual, expectedText)
+			})
+		})
+	})
+}
+
+/// now for Zdata (not Zdate)
+
+func TestCreationOfZData(t *testing.T) {
+	const n = 20
+	seg, _ := zdataFilledSegment(n)
+	text := CapnpDecodeSegment(seg, "", "test.capnp", "Z")
+
+	expectedText := `(zdata = (data = "\x00\x01\x02\x03\x04\x05\x06\a\b\t\n\v\f\r\x0e\x0f\x10\x11\x12\x13"))`
+
+	cv.Convey("Given a go-capnproto created Zdata DATA element with n=20", t, func() {
+		cv.Convey("When we decode it with capnp", func() {
+			cv.Convey(fmt.Sprintf("Then we should get the expected text '%s'", expectedText), func() {
+				cv.So(text, cv.ShouldEqual, expectedText)
+			})
+			cv.Convey("And our data should contain Z_ZDATA with contents 0,1,2,...,n", func() {
+				z := ReadRootZ(seg)
+				cv.So(z.Which(), cv.ShouldEqual, Z_ZDATA)
+
+				var data []byte = z.Zdata().Data()
+				cv.So(len(data), cv.ShouldEqual, n)
+				for i := range data {
+					cv.So(data[i], cv.ShouldEqual, i)
+				}
+
+			})
+		})
+	})
+
+}
