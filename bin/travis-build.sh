4bdbe022fa68b2fed107ed28eb92239a62dd952d
Ross Light <light@google.com>
2015-04-02 20:16:06 -0700

    travis: add update-alternatives for gcc

--- bin/travis-build.sh
+++ bin/travis-build.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 set -ev
 
+# Install gcc
+sudo apt-get install -qq g++-4.8 libstdc++-4.8-dev
+sudo update-alternatives --quiet --install /usr/bin/gcc  gcc  /usr/bin/gcc-4.8  60 --slave   /usr/bin/g++  g++  /usr/bin/g++-4.8 --slave   /usr/bin/gcov gcov /usr/bin/gcov-4.8
+sudo update-alternatives --quiet --set gcc /usr/bin/gcc-4.8
+
 # Install capnp
-export CXX=g++-4.9
 cd "$HOME"
 wget -O capnproto.tar.gz https://capnproto.org/capnproto-c++-0.5.1.2.tar.gz
 tar zxf capnproto.tar.gz
