77cd36e8c88117776cf7c0789a127d4a57a53ec5
Ross Light <light@google.com>
2015-10-12 12:40:10 -0700

    capnp: remove integration Decode tests
    Now that there are tighter unit tests for Marshal/Unmarshal/Encode/Decode,
    the old integration tests aren't as useful.

--- integration_test.go
+++ integration_test.go
@@ -361,50 +361,6 @@ func TestInterfaceCopyToOtherMessageWithCaps(t *testing.T) {
 	}
 }
 
-func TestDecode(t *testing.T) {
-	const n = 10
-	r := zdateReader(n, false)
-	msg, err := capnp.NewDecoder(r).Decode()
-	if err != nil {
-		t.Fatalf("Decode: %v", err)
-	}
-	z, err := air.ReadRootZ(msg)
-	if err != nil {
-		t.Fatalf("ReadRootZ: %v", err)
-	}
-	if z.Which() != air.Z_Which_zdatevec {
-		panic("expected Z_ZDATEVEC in root Z of segment")
-	}
-}
-
-func TestDecodeBackToBack(t *testing.T) {
-	const n = 10
-
-	r := zdateReaderNBackToBack(n, false)
-	d := capnp.NewDecoder(r)
-
-	for i := 0; i < n; i++ {
-		_, err := d.Decode()
-		if err != nil {
-			t.Fatalf("Decode: %v", err)
-		}
-	}
-}
-
-func TestPackedDecode(t *testing.T) {
-	const n = 10
-
-	r := zdateReaderNBackToBack(n, true)
-	d := capnp.NewPackedDecoder(r)
-
-	for i := 0; i < n; i++ {
-		_, err := d.Decode()
-		if err != nil {
-			t.Fatalf("Decode: %v, i=%d", err, i)
-		}
-	}
-}
-
 // demonstrate and test serialization to List(List(Struct(List))), nested lists.
 
 // start with smaller Struct(List)
