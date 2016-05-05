c634ead10ffeb9defe1840d79a968d22beb89d2f
Ross Light <ross@zombiezen.com>
2016-05-05 15:54:01 -0700

    travis: use go_import_path instead of custom install script

--- travis-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-# Install go-capnproto
-export GOPATH="$HOME/gopath"
-mkdir -p "$GOPATH/src/zombiezen.com/go"
-mv "$TRAVIS_BUILD_DIR" "$GOPATH/src/zombiezen.com/go/capnproto2"
-go get -v -t -d zombiezen.com/go/capnproto2/...
