e957493575e2045dfa2be9bb6c8a8fe2f84fd2b3
Ross Light <light@google.com>
2016-05-09 09:24:48 -0700

    new regen script to automate capnpc-go builds

---
+++ regen.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# regen.sh - update capnpc-go and regenerate schemas
+set -euo pipefail
+
+cd "$(dirname "$0")"
+
+echo "** mktemplates"
+(cd internal/cmd/mktemplates && go build -tags=mktemplates)
+
+echo "** capnpc-go"
+# Run tests so that we don't install a broken capnpc-go.
+(cd capnpc-go && go generate && go test && go install)
+
+echo "** schemas"
+go generate ./...
