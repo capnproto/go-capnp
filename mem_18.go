0cbfe5c64889126db41b99fb436a4b79b7c8ffe0
Tom Thorogood <me+github@tomthorogood.co.uk>
2017-03-18 08:49:34 +1030

    Use the same signature for (*Encoder).write

--- mem_18.go
+++ mem_18.go
@@ -4,7 +4,7 @@ package capnp
 
 import "net"
 
-func (e *Encoder) write(bufs net.Buffers) error {
-	_, err := bufs.WriteTo(e.w)
+func (e *Encoder) write(bufs [][]byte) error {
+	_, err := (*net.Buffers)(&bufs).WriteTo(e.w)
 	return err
 }
