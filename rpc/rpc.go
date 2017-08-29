4f39359c57d86bc46183ef19fb012923ec9557c5
Ross Light <light@google.com>
2017-08-28 19:09:07 -0700

    rpc: basic handling of returns
    Add receiver goroutine and factor out newMessage method.

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -23,6 +23,7 @@ type Conn struct {
 	mu sync.Mutex
 
 	closed     bool
+	recvDone   <-chan struct{}
 	recvCloser interface {
 		CloseRecv() error
 	}
@@ -32,6 +33,8 @@ type Conn struct {
 	// the channel is closed when the message is finished.
 	sending <-chan struct{}
 	send    Sender
+
+	nextQuestionID uint32
 }
 
 // Options specifies optional parameters for creating a Conn.
@@ -42,20 +45,28 @@ type Options struct {
 // transport.  Closing the connection will close the transport.
 // Passing nil for opts is the same as passing the zero value.
 func NewConn(t Transport, opts *Options) *Conn {
-	return &Conn{
+	done := make(chan struct{})
+	c := &Conn{
 		send:       t,
 		recvCloser: t,
+		recvDone:   done,
 	}
+	go func() {
+		defer close(done)
+		c.receive(context.Background(), t)
+	}()
+	return c
 }
 
-// Bootstrap returns the remote vat's bootstrap interface.
-func (c *Conn) Bootstrap(ctx context.Context) *capnp.Client {
-	c.mu.Lock()
+// newMessage creates a new outgoing message.  The caller must be
+// holding onto c.mu, but if newMessage does not return an error, then
+// c.mu will be released.  Once send or cancel is called, then c.mu
+// will be reacquired.
+func (c *Conn) newMessage(ctx context.Context) (_ rpccp.Message, send func() error, cancel func(), _ error) {
 	for {
 		if c.closed {
-			c.mu.Unlock()
 			// TODO(someday): classify as disconnected
-			return capnp.ErrorClient(errors.New("rpc bootstrap: connection closed"))
+			return rpccp.Message{}, nil, nil, errors.New("connection closed")
 		}
 		s := c.sending
 		if s == nil {
@@ -65,21 +76,42 @@ func (c *Conn) Bootstrap(ctx context.Context) *capnp.Client {
 		select {
 		case <-s:
 		case <-ctx.Done():
-			return capnp.ErrorClient(fmt.Errorf("rpc bootstrap: %v", ctx.Err()))
+			c.mu.Lock()
+			return rpccp.Message{}, nil, nil, ctx.Err()
 		}
 		c.mu.Lock()
 	}
 	sending := make(chan struct{})
 	c.sending = sending
 	c.mu.Unlock()
-	defer func() {
+	msg, tsend, tcancel, err := c.send.NewMessage(ctx)
+	if err != nil {
 		c.mu.Lock()
 		close(sending)
 		c.sending = nil
-		c.mu.Unlock()
-	}()
+		return rpccp.Message{}, nil, nil, err
+	}
+	return msg, func() error {
+			err := tsend()
+			c.mu.Lock()
+			close(sending)
+			c.sending = nil
+			return err
+		}, func() {
+			tcancel()
+			c.mu.Lock()
+			close(sending)
+			c.sending = nil
+		}, nil
+}
 
-	msg, send, cancel, err := c.send.NewMessage(ctx)
+// Bootstrap returns the remote vat's bootstrap interface.
+func (c *Conn) Bootstrap(ctx context.Context) *capnp.Client {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	id := c.nextQuestionID
+	c.nextQuestionID++
+	msg, send, cancel, err := c.newMessage(ctx)
 	if err != nil {
 		return capnp.ErrorClient(fmt.Errorf("rpc bootstrap: create message: %v", err))
 	}
@@ -88,8 +120,7 @@ func (c *Conn) Bootstrap(ctx context.Context) *capnp.Client {
 		cancel()
 		return capnp.ErrorClient(fmt.Errorf("rpc bootstrap: create message: %v", err))
 	}
-	// TODO(soon): allocate an ID
-	boot.SetQuestionId(0)
+	boot.SetQuestionId(id)
 	if err := send(); err != nil {
 		return capnp.ErrorClient(fmt.Errorf("rpc bootstrap: send message: %v", err))
 	}
@@ -103,7 +134,7 @@ func (c *Conn) Close() error {
 	c.mu.Lock()
 	c.closed = true
 	c.mu.Unlock()
-	rerr := c.recvCloser.CloseRecv()
+	rerr := c.recvCloser.CloseRecv() // will wait on recvDone at the end
 
 	// Close Sender after all sends are finished.
 	c.mu.Lock()
@@ -140,6 +171,7 @@ func (c *Conn) Close() error {
 closeSend:
 	serr := c.send.CloseSend()
 
+	<-c.recvDone
 	if rerr != nil {
 		return fmt.Errorf("rpc: close transport: %v", rerr)
 	}
@@ -147,4 +179,75 @@ closeSend:
 		return fmt.Errorf("rpc: close transport: %v", serr)
 	}
 	return nil
+}
+
+// receive receives and dispatches messages coming from r.
+// It is intended to run in its own goroutine.
+func (c *Conn) receive(ctx context.Context, r Receiver) {
+	for {
+		recv, releaseRecv, err := r.RecvMessage(ctx)
+		if err != nil {
+			// TODO(soon): log error
+			return
+		}
+		switch recv.Which() {
+		case rpccp.Message_Which_unimplemented:
+			// no-op for now to avoid feedback loop
+		case rpccp.Message_Which_return:
+			ret, err := recv.Return()
+			if err != nil {
+				// TODO(soon): log error
+				continue
+			}
+			err = c.handleReturn(ctx, ret, releaseRecv)
+			if err != nil {
+				// TODO(soon): log error
+				continue
+			}
+		default:
+			err := c.handleUnimplemented(ctx, recv)
+			releaseRecv()
+			if err != nil {
+				// TODO(soon): log error
+				continue
+			}
+		}
+	}
+}
+
+func (c *Conn) handleReturn(ctx context.Context, ret rpccp.Return, releaseRet capnp.ReleaseFunc) error {
+	defer releaseRet()
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	// TODO(soon): disconnect if return ID not in questions table.
+	msg, send, cancel, err := c.newMessage(ctx)
+	if err != nil {
+		return err
+	}
+	fin, err := msg.NewFinish()
+	if err != nil {
+		cancel()
+		return err
+	}
+	fin.SetQuestionId(ret.AnswerId())
+	fin.SetReleaseResultCaps(false)
+	if err := send(); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Conn) handleUnimplemented(ctx context.Context, recv rpccp.Message) error {
+	defer c.mu.Unlock()
+	c.mu.Lock()
+	msg, send, cancel, err := c.newMessage(ctx)
+	if err != nil {
+		return err
+	}
+	if err := msg.SetUnimplemented(recv); err != nil {
+		cancel()
+		return nil
+	}
+	err = send()
+	return err
 }
--- rpc/rpc_test.go
+++ rpc/rpc_test.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"testing"
 
+	"zombiezen.com/go/capnproto2"
 	"zombiezen.com/go/capnproto2/pogs"
 	"zombiezen.com/go/capnproto2/rpc"
 	rpccapnp "zombiezen.com/go/capnproto2/std/capnp/rpc"
@@ -56,7 +57,9 @@ func TestBootstrap(t *testing.T) {
 	}()
 
 	ctx := context.Background()
-	conn.Bootstrap(ctx)
+
+	// 1. Read bootstrap
+	client := conn.Bootstrap(ctx)
 	msg, release, err := p2.RecvMessage(ctx)
 	if err != nil {
 		t.Fatal("p2.RecvMessage:", err)
@@ -69,6 +72,60 @@ func TestBootstrap(t *testing.T) {
 	if rmsg.Which != rpccapnp.Message_Which_bootstrap {
 		t.Fatalf("Received %v message; want bootstrap", rmsg.Which)
 	}
+	qid := rmsg.Bootstrap.QuestionID
+
+	// 2. Write back a return
+	msg, send, cancel, err := p2.NewMessage(ctx)
+	if err != nil {
+		t.Fatal("p2.NewMessage():", err)
+	}
+	iptr := capnp.NewInterface(msg.Segment(), 0)
+	err = pogs.Insert(rpccapnp.Message_TypeID, msg.Struct, &rpcMessage{
+		Which: rpccapnp.Message_Which_return,
+		Return: &rpcReturn{
+			AnswerID: qid,
+			Which:    rpccapnp.Return_Which_results,
+			Results: &rpcPayload{
+				Content: iptr.ToPtr(),
+				CapTable: []rpcCapDescriptor{
+					{
+						Which:        rpccapnp.CapDescriptor_Which_senderHosted,
+						SenderHosted: bootstrapExportID,
+					},
+				},
+			},
+		},
+	})
+	if err != nil {
+		cancel()
+		t.Fatal("pogs.Insert(p2.NewMessage(), &rpcMessage{...}):", err)
+	}
+	if err := send(); err != nil {
+		t.Fatal("send():", err)
+	}
+
+	// 3. Read finish after client is resolved.
+	if err := client.Resolve(ctx); err != nil {
+		t.Error("client.Resolve:", err)
+	}
+	msg, release, err = p2.RecvMessage(ctx)
+	if err != nil {
+		t.Fatal("p2.RecvMessage:", err)
+	}
+	defer release()
+	rmsg = rpcMessage{}
+	if err := pogs.Extract(&rmsg, rpccapnp.Message_TypeID, msg.Struct); err != nil {
+		t.Fatal("pogs.Extract(p2.RecvMessage(ctx)):", err)
+	}
+	if rmsg.Which != rpccapnp.Message_Which_finish {
+		t.Fatalf("Received %v message; want finish", rmsg.Which)
+	}
+	if rmsg.Finish.QuestionID != qid {
+		t.Errorf("Received finish for question %d; want %d", rmsg.Finish.QuestionID, qid)
+	}
+	if rmsg.Finish.ReleaseResultCaps {
+		t.Error("Received finish that releases bootstrap result capabilities")
+	}
 }
 
 type rpcMessage struct {
@@ -76,6 +133,8 @@ type rpcMessage struct {
 	Unimplemented *rpcMessage
 	Abort         *rpcException
 	Bootstrap     *rpcBootstrap
+	Return        *rpcReturn
+	Finish        *rpcFinish
 }
 
 type rpcException struct {
@@ -85,4 +144,41 @@ type rpcException struct {
 
 type rpcBootstrap struct {
 	QuestionID uint32 `capnp:"questionId"`
+}
+
+type rpcReturn struct {
+	AnswerID         uint32 `capnp:"answerId"`
+	ReleaseParamCaps bool
+
+	Which                 rpccapnp.Return_Which
+	Results               *rpcPayload
+	Exception             *rpcException
+	TakeFromOtherQuestion uint32
+}
+
+type rpcFinish struct {
+	QuestionID        uint32 `capnp:"questionId"`
+	ReleaseResultCaps bool
+}
+
+type rpcPayload struct {
+	Content  capnp.Ptr
+	CapTable []rpcCapDescriptor
+}
+
+type rpcCapDescriptor struct {
+	Which          rpccapnp.CapDescriptor_Which
+	SenderHosted   uint32
+	SenderPromise  uint32
+	ReceiverHosted uint32
+}
+
+type rpcPromisedAnswer struct {
+	QuestionID uint32 `capnp:"questionId"`
+	Transform  []rpcPromisedAnswerOp
+}
+
+type rpcPromisedAnswerOp struct {
+	Which           rpccapnp.PromisedAnswer_Op_Which
+	GetPointerField uint16
 }
