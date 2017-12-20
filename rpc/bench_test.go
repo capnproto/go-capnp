f7bfb3883e134908796cf6d3f37bdc2f5f5cfd11
Ross Light <light@google.com>
2017-12-20 13:20:51 -0800

    rpc: move rpc_test to level0_test

---
+++ rpc/bench_test.go
@@ -0,0 +1,66 @@
+package rpc_test
+
+import (
+	"context"
+	"testing"
+
+	"zombiezen.com/go/capnproto2/rpc"
+	testcp "zombiezen.com/go/capnproto2/rpc/internal/testcapnp"
+)
+
+func BenchmarkPingPong(b *testing.B) {
+	p1, p2 := newPipe(1)
+	srv := testcp.PingPong_ServerToClient(pingPongServer{}, nil)
+	conn1 := rpc.NewConn(p2, &rpc.Options{
+		ErrorReporter:   testErrorReporter{tb: b},
+		BootstrapClient: srv.Client,
+	})
+	defer func() {
+		<-conn1.Done()
+		if err := conn1.Close(); err != nil {
+			b.Error("conn1.Close:", err)
+		}
+	}()
+	conn2 := rpc.NewConn(p1, &rpc.Options{
+		ErrorReporter: testErrorReporter{tb: b},
+	})
+	defer func() {
+		if err := conn2.Close(); err != nil {
+			b.Error("conn2.Close:", err)
+		}
+	}()
+
+	ctx := context.Background()
+	client := testcp.PingPong{Client: conn2.Bootstrap(ctx)}
+	defer client.Client.Release()
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		ans, release := client.EchoNum(ctx, func(args testcp.PingPong_echoNum_Params) error {
+			args.SetN(42)
+			return nil
+		})
+		result, err := ans.Struct()
+		if err != nil {
+			release()
+			b.Errorf("call failed on iteration %d: %v", i, err)
+			break
+		}
+		n := result.N()
+		release()
+		if n != 42 {
+			b.Errorf("n = %d; want 42", n)
+			break
+		}
+	}
+}
+
+type pingPongServer struct{}
+
+func (pingPongServer) EchoNum(ctx context.Context, call testcp.PingPong_echoNum) error {
+	out, err := call.AllocResults()
+	if err != nil {
+		return err
+	}
+	out.SetN(call.Args().N())
+	return nil
+}
