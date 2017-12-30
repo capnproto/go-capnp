5df1aa48eb8cd641f63fb29172fe1087a6ec961d
Ross Light <light@google.com>
2017-12-30 15:19:28 -0800

    capnp: mem.go -> message.go, capn.go -> segment.go
    Reflects what's actually in the files.

---
+++ error.go
@@ -0,0 +1,49 @@
+package capnp
+
+import (
+	"fmt"
+
+	"zombiezen.com/go/capnproto2/internal/errors"
+)
+
+// Unimplemented returns an error that formats as the given text and
+// will report true when passed to IsUnimplemented.
+func Unimplemented(s string) error {
+	return errors.New(errors.Unimplemented, "", s)
+}
+
+// IsUnimplemented reports whether e indicates that functionality is unimplemented.
+func IsUnimplemented(e error) bool {
+	return errors.TypeOf(e) == errors.Unimplemented
+}
+
+// Disconnected returns an error that formats as the given text and
+// will report true when passed to IsDisconnected.
+func Disconnected(s string) error {
+	return errors.New(errors.Disconnected, "", s)
+}
+
+// IsDisconnected reports whether e indicates a failure due to loss of a necessary capability.
+func IsDisconnected(e error) bool {
+	return errors.TypeOf(e) == errors.Disconnected
+}
+
+func newError(msg string) error {
+	return errors.New(errors.Failed, "capnp", msg)
+}
+
+func errorf(format string, args ...interface{}) error {
+	return newError(fmt.Sprintf(format, args...))
+}
+
+type annotater struct {
+	err error
+}
+
+func annotate(err error) annotater {
+	return annotater{err}
+}
+
+func (a annotater) errorf(format string, args ...interface{}) error {
+	return errors.Annotate("capnp", fmt.Sprintf(format, args...), a.err)
+}
