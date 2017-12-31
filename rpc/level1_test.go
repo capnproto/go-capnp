e289567e3a13065dac59a7bbaa5e53d5d7ff8165
Ross Light <light@google.com>
2017-12-31 09:21:25 -0800

    rpc: add issue 3 regression test

--- rpc/level1_test.go
+++ rpc/level1_test.go
@@ -304,6 +304,277 @@ func TestRecvDisembargo(t *testing.T) {
 	}
 }
 
+// TestIssue3 exposes a capability that makes a call to its received
+// capability argument, acks the call, then waits on its return.  In
+// earlier versions of go-capnproto, this would cause a deadlock.
+// See https://github.com/capnproto/go-capnproto2/issues/3 for history.
+// Level 1 requirement.
+func TestIssue3(t *testing.T) {
+	const callbackInterfaceID = interfaceID + 100
+	const callbackMethodID = 9
+	srv := capnp.NewClient(server.New([]server.Method{
+		{
+			Method: capnp.Method{
+				InterfaceID: interfaceID,
+				MethodID:    methodID,
+			},
+			Impl: func(ctx context.Context, call *server.Call) error {
+				in, err := call.Args().Ptr(0)
+				if err != nil {
+					return fmt.Errorf("capability arg: %v", err)
+				}
+				ans, release := in.Interface().Client().SendCall(ctx, capnp.Send{
+					Method: capnp.Method{
+						InterfaceID: callbackInterfaceID,
+						MethodID:    callbackMethodID,
+					},
+				})
+				defer release()
+				call.Ack()
+				callbackResult, err := ans.Struct()
+				if err != nil {
+					return fmt.Errorf("callback: %v", err)
+				}
+				res, err := call.AllocResults(callbackResult.Size())
+				if err != nil {
+					return err
+				}
+				if err := res.CopyFrom(callbackResult); err != nil {
+					return fmt.Errorf("copy to result: %v", err)
+				}
+				return nil
+			},
+		},
+	}, nil /* brand */, nil /* shutdown */, nil /* policy */))
+	p1, p2 := newPipe(1)
+	defer p2.Close()
+	conn := rpc.NewConn(p1, &rpc.Options{
+		BootstrapClient: srv,
+		ErrorReporter:   testErrorReporter{tb: t},
+	})
+	defer func() {
+		if err := conn.Close(); err != nil {
+			t.Error(err)
+		}
+	}()
+	ctx := context.Background()
+
+	// 1. Write bootstrap
+	const bootstrapQID = 54
+	{
+		msg := &rpcMessage{
+			Which:     rpccp.Message_Which_bootstrap,
+			Bootstrap: &rpcBootstrap{QuestionID: bootstrapQID},
+		}
+		if err := sendMessage(ctx, p2, msg); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// 2. Read bootstrap return.
+	bootstrapImportID, err := recvBootstrapReturn(ctx, p2, bootstrapQID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 3. Write bootstrap finish
+	{
+		msg := &rpcMessage{
+			Which: rpccp.Message_Which_finish,
+			Finish: &rpcFinish{
+				QuestionID:        bootstrapQID,
+				ReleaseResultCaps: false,
+			},
+		}
+		if err := sendMessage(ctx, p2, msg); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	// 4. Write call with an export
+	const callQID = 55
+	const exportID = 888
+	{
+		msg, send, release, err := p2.NewMessage(ctx)
+		if err != nil {
+			t.Fatal("p2.NewMessage():", err)
+		}
+		params, err := capnp.NewStruct(msg.Segment(), capnp.ObjectSize{PointerCount: 1})
+		if err != nil {
+			release()
+			t.Fatal("capnp.NewStruct:", err)
+		}
+		err = params.SetPtr(0, capnp.NewInterface(params.Segment(), 0).ToPtr())
+		if err != nil {
+			release()
+			t.Fatal("capnp.NewStruct.SetPtr:", err)
+		}
+		err = pogs.Insert(rpccp.Message_TypeID, msg.Struct, &rpcMessage{
+			Which: rpccp.Message_Which_call,
+			Call: &rpcCall{
+				QuestionID: callQID,
+				Target: rpcMessageTarget{
+					Which:       rpccp.MessageTarget_Which_importedCap,
+					ImportedCap: bootstrapImportID,
+				},
+				InterfaceID: interfaceID,
+				MethodID:    methodID,
+				Params: rpcPayload{
+					Content: params.ToPtr(),
+					CapTable: []rpcCapDescriptor{
+						{
+							Which:        rpccp.CapDescriptor_Which_senderHosted,
+							SenderHosted: exportID,
+						},
+					},
+				},
+			},
+		})
+		if err != nil {
+			release()
+			t.Fatal("pogs.Insert(p2.NewMessage(), &rpcMessage{...}):", err)
+		}
+		err = send()
+		release()
+		if err != nil {
+			t.Fatal("send():", err)
+		}
+	}
+
+	// 5. Read callback
+	var callbackQID uint32
+	{
+		msg, release, err := p2.RecvMessage(ctx)
+		if err != nil {
+			t.Fatal("p2.RecvMessage:", err)
+		}
+		var rmsg rpcMessage
+		err = pogs.Extract(&rmsg, rpccp.Message_TypeID, msg.Struct)
+		release()
+		if err != nil {
+			t.Fatal("pogs.Extract(p2.RecvMessage(ctx)):", err)
+		}
+		if rmsg.Which != rpccp.Message_Which_call {
+			t.Fatalf("Received %v message; want call", rmsg.Which)
+		}
+		callbackQID = rmsg.Call.QuestionID
+		if rmsg.Call.InterfaceID != callbackInterfaceID || rmsg.Call.MethodID != callbackMethodID {
+			got := capnp.Method{InterfaceID: rmsg.Call.InterfaceID, MethodID: rmsg.Call.MethodID}
+			want := capnp.Method{InterfaceID: callbackInterfaceID, MethodID: callbackMethodID}
+			t.Fatalf("Received call for %v; want %v", got, want)
+		}
+		if rmsg.Call.Target.Which != rpccp.MessageTarget_Which_importedCap {
+			t.Fatalf("Received call on %v; want importedCap", rmsg.Call.Target.Which)
+		}
+		if rmsg.Call.Target.ImportedCap != exportID {
+			t.Fatalf("Received call on export %d; want %d", rmsg.Call.Target.ImportedCap, exportID)
+		}
+	}
+
+	// 6. Write callback return.
+	{
+		msg, send, release, err := p2.NewMessage(ctx)
+		if err != nil {
+			t.Fatal("p2.NewMessage():", err)
+		}
+		results, err := capnp.NewStruct(msg.Segment(), capnp.ObjectSize{DataSize: 8})
+		if err != nil {
+			release()
+			t.Fatal("capnp.NewStruct:", err)
+		}
+		results.SetUint64(0, 0xdeadbeef)
+		err = pogs.Insert(rpccp.Message_TypeID, msg.Struct, &rpcMessage{
+			Which: rpccp.Message_Which_return,
+			Return: &rpcReturn{
+				AnswerID:         callbackQID,
+				ReleaseParamCaps: true,
+				Which:            rpccp.Return_Which_results,
+				Results:          &rpcPayload{Content: results.ToPtr()},
+			},
+		})
+		if err != nil {
+			release()
+			t.Fatal("pogs.Insert(p2.NewMessage(), &rpcMessage{...}):", err)
+		}
+		err = send()
+		release()
+		if err != nil {
+			t.Fatal("send():", err)
+		}
+	}
+
+	// 7. Read callback finish and call return (and possibly a release).
+	seenFinish := false
+	seenReturn := false
+	exportRefs := 1
+	for !seenFinish || !seenReturn {
+		msg, release, err := p2.RecvMessage(ctx)
+		if err != nil {
+			t.Fatal("p2.RecvMessage:", err)
+		}
+		defer release()
+		var rmsg rpcMessage
+		if err := pogs.Extract(&rmsg, rpccp.Message_TypeID, msg.Struct); err != nil {
+			t.Fatal("pogs.Extract(p2.RecvMessage(ctx)):", err)
+		}
+		switch rmsg.Which {
+		case rpccp.Message_Which_return:
+			if seenReturn {
+				t.Fatal("Received two returns")
+			}
+			seenReturn = true
+			if rmsg.Return.AnswerID != callQID {
+				t.Errorf("Received return for question %d; want %d", rmsg.Return.AnswerID, callQID)
+			}
+			switch rmsg.Return.Which {
+			case rpccp.Return_Which_results:
+				content := rmsg.Return.Results.Content.Struct()
+				if content.Uint64(0) != 0xdeadbeef {
+					t.Errorf("Call result = %#x; want 0xdeadbeef", content.Uint64(0))
+				}
+			case rpccp.Return_Which_exception:
+				t.Errorf("Call exception: %s", rmsg.Return.Exception.Reason)
+			default:
+				t.Fatalf("Received %v return; want results", rmsg.Return.Which)
+			}
+		case rpccp.Message_Which_finish:
+			if seenFinish {
+				t.Fatal("Received two finishes")
+			}
+			seenFinish = true
+			if rmsg.Finish.QuestionID != callbackQID {
+				t.Errorf("Received finish for question %d; want %d", rmsg.Finish.QuestionID, callbackQID)
+			}
+		case rpccp.Message_Which_release:
+			if rmsg.Release.ID != exportID {
+				t.Errorf("Received release for export ID %d; want %d", rmsg.Release.ID, exportID)
+			} else if rmsg.Release.ReferenceCount > uint32(exportRefs) {
+				t.Errorf("Released %d references for export, but only have %d", rmsg.Release.ReferenceCount, exportRefs)
+				exportRefs = 0
+			} else {
+				exportRefs -= int(rmsg.Release.ReferenceCount)
+			}
+		default:
+			t.Fatalf("Received %v message; wanted return, finish, or release", rmsg.Which)
+		}
+		release()
+	}
+
+	// 8. Write finish
+	{
+		msg := &rpcMessage{
+			Which: rpccp.Message_Which_finish,
+			Finish: &rpcFinish{
+				QuestionID:        callQID,
+				ReleaseResultCaps: false,
+			},
+		}
+		if err := sendMessage(ctx, p2, msg); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
 type rpcResolve struct {
 	PromiseID uint32 `capnp:"promiseId"`
 	Which     rpccp.Resolve_Which
