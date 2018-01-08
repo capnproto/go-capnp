dfff78f1e3c17df53b9930679e8ba6524a4fd450
Ross Light <light@google.com>
2018-01-08 15:33:19 -0800

    rpc: only run pipe transport test on Go >=1.9
    Fixes test breakage.  See https://golang.org/issue/7970 for more
    details.

--- rpc/transport_test.go
+++ rpc/transport_test.go
@@ -3,9 +3,7 @@ package rpc_test
 import (
 	"context"
 	"errors"
-	"io"
 	"net"
-	"os"
 	"testing"
 	"time"
 
@@ -227,67 +225,4 @@ func TestTCPStreamTransport(t *testing.T) {
 			return
 		})
 	})
-}
-
-func TestFileStreamTransport(t *testing.T) {
-	testTransport(t, func() (t1, t2 rpc.Transport, err error) {
-		r1, w1, err := os.Pipe()
-		if err != nil {
-			return nil, nil, err
-		}
-		r2, w2, err := os.Pipe()
-		if err != nil {
-			r1.Close()
-			w1.Close()
-			return nil, nil, err
-		}
-		t1 = rpc.NewStreamTransport(readWriteCloser{r1, w2})
-		t2 = rpc.NewStreamTransport(readWriteCloser{r2, w1})
-		return t1, t2, nil
-	})
-}
-
-type readWriteCloser struct {
-	r io.ReadCloser
-	w io.WriteCloser
-}
-
-func (rwc readWriteCloser) Read(p []byte) (int, error) {
-	return rwc.r.Read(p)
-}
-
-func (rwc readWriteCloser) SetReadDeadline(t time.Time) error {
-	d, ok := rwc.r.(interface {
-		SetReadDeadline(time.Time) error
-	})
-	if !ok {
-		return errors.New("read deadline not implemented")
-	}
-	return d.SetReadDeadline(t)
-}
-
-func (rwc readWriteCloser) Write(p []byte) (int, error) {
-	return rwc.w.Write(p)
-}
-
-func (rwc readWriteCloser) SetWriteDeadline(t time.Time) error {
-	d, ok := rwc.w.(interface {
-		SetWriteDeadline(time.Time) error
-	})
-	if !ok {
-		return errors.New("write deadline not implemented")
-	}
-	return d.SetWriteDeadline(t)
-}
-
-func (rwc readWriteCloser) Close() error {
-	werr := rwc.w.Close()
-	rerr := rwc.r.Close()
-	if werr != nil {
-		return werr
-	}
-	if rerr != nil {
-		return rerr
-	}
-	return nil
 }
