307eccb80c991d65af6cea4df612a5835a05efad
Ian Denhardt <ian@zenhack.net>
2021-12-06 01:43:43 -0500

    First actual steps on flow control.
    This defines an interface for limiters, and a limiter that just sets a
    fixed bound on in-flight message sizes. Still TODO:
    
    - Document
    - Test
    - Integrate these with the rpc system
    - Later: add an adaptive implementation that tries to measure BDP.

---
+++ flowcontrol/flowlimiter.go
@@ -0,0 +1,100 @@
+package flowcontrol
+
+import (
+	"context"
+	"sync"
+
+	"capnproto.org/go/capnp/v3/internal/chanmutex"
+)
+
+type FlowLimiter interface {
+	StartMessage(ctx context.Context, size uint64) (gotResponse func(), err error)
+}
+
+func NewFixedLimiter(size uint64) FlowLimiter {
+	return &fixedLimiter{
+		total: size,
+		avail: size,
+	}
+}
+
+type fixedLimiter struct {
+	mu           sync.Mutex
+	total, avail uint64
+
+	pending requestQueue
+}
+
+func (fl *fixedLimiter) StartMessage(ctx context.Context, size uint64) (gotResponse func(), err error) {
+	gotResponse = fl.makeCallback(size)
+	fl.mu.Lock()
+	ready := fl.pending.put(size)
+	fl.pumpQueue()
+	fl.mu.Unlock()
+	select {
+	case <-ready:
+		return gotResponse, nil
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+// must be holding mu
+func (fl *fixedLimiter) pumpQueue() {
+	next := fl.pending.peek()
+	for next != nil && next.size <= fl.avail {
+		fl.pending.get() // actually de-queue it.
+		next.ready <- struct{}{}
+		next = fl.pending.peek()
+	}
+}
+
+func (fl *fixedLimiter) makeCallback(size uint64) func() {
+	return func() {
+		fl.mu.Lock()
+		defer fl.mu.Unlock()
+		fl.avail += size
+		fl.pumpQueue()
+	}
+}
+
+type requestQueue struct {
+	head, tail *request
+}
+
+type request struct {
+	ready chanmutex.Mutex
+	size  uint64
+	next  *request
+}
+
+func (q *requestQueue) peek() *request {
+	return q.head
+}
+
+func (q *requestQueue) get() *request {
+	ret := q.head
+	if ret != nil {
+		q.head = ret.next
+	}
+	if q.head == nil {
+		q.tail = nil
+	}
+	return ret
+}
+
+func (q *requestQueue) put(size uint64) chanmutex.Mutex {
+	req := &request{
+		ready: chanmutex.NewUnlocked(),
+		size:  size,
+		next:  nil,
+	}
+	if q.tail == nil {
+		q.tail = req
+		q.head = req
+	} else {
+		q.tail.next = req
+		q.tail = req
+	}
+	return req.ready
+}
