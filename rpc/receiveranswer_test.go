c243a766bffe7c50ed2acc4d0b235a6f31c3de98
Ian Denhardt <ian@zenhack.net>
2021-12-29 19:37:37 -0500

    Fill in the rest of the test.
    ...which fails; need to debug.

--- rpc/receiveranswer_test.go
+++ rpc/receiveranswer_test.go
@@ -2,7 +2,10 @@ package rpc
 
 import (
 	"context"
+	"fmt"
 	"net"
+	"os"
+	"syscall"
 	"testing"
 
 	//"github.com/stretchr/testify/assert"
@@ -13,26 +16,74 @@ import (
 	"capnproto.org/go/capnp/v3/server"
 )
 
+// A variant of net.Pipe() that uses the socketpair() syscall, instead of
+// using an in-proces transport. This is also buffered, which works around
+// #189. TODO: once that issue is fixed, delete this and just use net.Pipe().
+func netPipe() (net.Conn, net.Conn) {
+	fds, err := syscall.Socketpair(syscall.AF_UNIX, syscall.SOCK_STREAM, 0)
+	chkfatal(err)
+	mkConn := func(fd int, name string) net.Conn {
+		conn, err := net.FileConn(os.NewFile(uintptr(fd), name))
+		chkfatal(err)
+		return conn
+	}
+	return mkConn(fds[0], "pipe0"), mkConn(fds[1], "pipe1")
+}
+
+type capArgsTest struct {
+	Errs chan<- error
+}
+
+func (me *capArgsTest) Call(ctx context.Context, p testcapnp.CapArgsTest_call) error {
+	defer close(me.Errs)
+	cap, err := p.Args().Cap()
+	chkfatal(err)
+	client := cap.Interface().Client()
+	chkfatal(client.Resolve(ctx))
+	brand, ok := server.IsServer(client.State().Brand)
+	if !ok {
+		err := fmt.Errorf("server.IsServer returned !ok")
+		me.Errs <- err
+		return err
+	}
+	other := brand.(*capArgsTest)
+	if other != me {
+		me.Errs <- fmt.Errorf(
+			"Passed something other than ourselves: wanted %v but got %v",
+			me, other)
+	}
+	return nil
+}
+
+func chkfatal(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
 func TestCallBootstrapReceiverAnswer(t *testing.T) {
-	cClient, cServer := net.Pipe()
+	cClient, cServer := netPipe()
+	defer cClient.Close()
+	defer cServer.Close()
+
+	errChan := make(chan error)
+	srv := &capArgsTest{Errs: errChan}
 
 	conn := NewConn(
 		NewStreamTransport(cServer),
 		&Options{
-			BootstrapClient: testcapnp.PingPong_ServerToClient(nil, &server.Policy{
-				MaxConcurrentCalls: 10,
-			}).Client,
-		})
+			BootstrapClient: testcapnp.CapArgsTest_ServerToClient(
+				srv,
+				&server.Policy{
+					MaxConcurrentCalls: 10,
+				},
+			).Client,
+		},
+	)
 	defer conn.Close()
 	trans := NewStreamTransport(cClient)
 
 	ctx := context.Background()
-
-	chkfatal := func(err error) {
-		if err != nil {
-			panic(err)
-		}
-	}
 
 	msg, send, release, err := trans.NewMessage(ctx)
 	chkfatal(err)
@@ -69,4 +120,8 @@ func TestCallBootstrapReceiverAnswer(t *testing.T) {
 	params.SetContent(capnp.NewInterface(params.Struct.Segment(), 0).ToPtr())
 	send()
 	release()
+
+	for err = range errChan {
+		t.Errorf("Error: %v", err)
+	}
 }
