1e764971afca35d50a32ddc3520ea78337d3befa
Louis Thibault <l.thibault@sentimens.com>
2022-07-15 22:01:49 +0200

    Add tests for pipe.

---
+++ rpc/transport/pipe_test.go
@@ -0,0 +1,28 @@
+package transport_test
+
+import (
+	"context"
+	"testing"
+
+	"capnproto.org/go/capnp/v3"
+	"capnproto.org/go/capnp/v3/rpc/transport"
+	"github.com/stretchr/testify/require"
+)
+
+func TestPipe(t *testing.T) {
+	t.Parallel()
+
+	m, _ := capnp.NewSingleSegmentMessage(nil)
+
+	p1, p2 := transport.NewPipe(1)
+
+	err := p1.Encode(context.Background(), m)
+	require.NoError(t, err)
+
+	m2, err := p2.Decode(context.Background())
+	require.NoError(t, err)
+	require.NotEqual(t, m, m2, "message should have been copied")
+
+	err = p1.Close()
+	require.NoError(t, err)
+}
