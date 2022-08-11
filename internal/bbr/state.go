072943cdccc8a764c32e06999c6d2c54b686e47b
Ian Denhardt <ian@zenhack.net>
2022-08-10 20:26:33 -0400

    More comments.

--- internal/bbr/state.go
+++ internal/bbr/state.go
@@ -6,14 +6,20 @@ import (
 	"capnproto.org/go/capnp/v3/internal/clock"
 )
 
-type packet struct {
-	SendTime   time.Time
-	Size       int64
+// A packetMeta contains metadata about a packet that was sent.
+type packetMeta struct {
+	SendTime time.Time // The time at which the packet was sent.
+	Size     int64     // The size of the packet.
+
+	// Whether the connection flow was app-limited when this packet
+	// was sent:
 	AppLimited bool
 }
 
 // A state tracks the state used by BBR to manage flow control.
 type state struct {
+	// Filters for estimating the round trip propagation time and
+	// bottleneck bandwidth, respectively:
 	rtPropFilter rtPropFilter
 	btlBwFilter  btlBwFilter
 
@@ -25,7 +31,9 @@ type state struct {
 	clock clock.Clock
 }
 
-func (s *state) onAck(p packet) {
+// onAck should be invoked on each packetMeta when the acknowledgement for
+// that packet is received.
+func (s *state) onAck(p packetMeta) {
 	now := s.clock.Now()
 	rtt := now.Sub(p.SendTime)
 
