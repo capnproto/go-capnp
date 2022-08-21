d0a9f5e404d89f922b193a3fcc1510a8b471ff1c
Ian Denhardt <ian@zenhack.net>
2022-08-20 20:52:41 -0400

    Add tests for manual clock/timer.

---
+++ internal/clock/clock_test.go
@@ -0,0 +1,59 @@
+package clock
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestTimers(t *testing.T) {
+	// arbitrary initial now:
+	init := time.Unix(1e9, 0)
+
+	clock := NewManual(init)
+
+	timer := clock.NewTimer(5 * time.Second)
+
+	select {
+	case <-timer.Chan():
+		t.Fatal("Timer went off too early.")
+	default:
+	}
+
+	clock.Advance(1 * time.Second)
+
+	select {
+	case <-timer.Chan():
+		t.Fatal("Timer went off too early.")
+	default:
+	}
+
+	clock.Advance(10 * time.Second)
+
+	select {
+	case <-timer.Chan():
+	default:
+		t.Fatal("Timer didn't go off.")
+	}
+
+	assert.False(t, timer.Stop(), "Timer should have already been stopped.")
+
+	assert.Equal(t, init.Add(11*time.Second), clock.Now(), "Time should be 11 seconds later.")
+
+	timer.Reset(1 * time.Second)
+
+	select {
+	case <-timer.Chan():
+		t.Fatal("Timer went off again too early.")
+	default:
+	}
+
+	clock.Advance(2 * time.Second)
+
+	select {
+	case <-timer.Chan():
+	default:
+		t.Fatal("Timer didn't go off.")
+	}
+}
