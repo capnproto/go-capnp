13ab3d269d65b30a3ab386edda66486ef586dedc
Ian Denhardt <ian@zenhack.net>
2022-08-22 15:59:20 -0400

    Add a test.

--- internal/bbr/limiter_test.go
+++ internal/bbr/limiter_test.go
@@ -6,7 +6,23 @@ import (
 	"time"
 
 	"capnproto.org/go/capnp/v3/internal/clock"
+
+	"github.com/stretchr/testify/assert"
 )
+
+func TestReleaseLimiter(t *testing.T) {
+	lim := NewLimiter(clock.System)
+	lim.Release()
+	select {
+	case <-lim.ctx.Done():
+	case <-time.NewTimer(10 * time.Millisecond).C:
+		t.Fatal("lim.Release did not cancel the context.")
+	}
+
+	_, err := lim.StartMessage(context.Background(), 1)
+	assert.NotNil(t, err, "Error should be non-nil if the limiter has shut down.")
+	assert.Equal(t, lim.ctx.Err(), err, "Error should be that of the limiter's context.")
+}
 
 // TODO: refine this, be more specific than "Stuff" wrt. what's being tested.
 func TestStuff(t *testing.T) {
