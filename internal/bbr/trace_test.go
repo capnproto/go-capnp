a79f1cddfc2642cad608dbb6b735123a5847fa68
Ian Denhardt <ian@zenhack.net>
2022-08-26 22:31:15 -0400

    Move logging out of the hot path.

--- internal/bbr/trace_test.go
+++ internal/bbr/trace_test.go
@@ -3,10 +3,42 @@ package bbr
 import (
 	"context"
 	"testing"
+	"time"
 
 	"capnproto.org/go/capnp/v3/internal/clock"
 	"github.com/stretchr/testify/assert"
 )
+
+type snapshot struct {
+	lim Limiter
+	now time.Time
+}
+
+func (s *snapshot) report(t *testing.T) {
+	lim := &s.lim
+	t.Logf("Limiter snapshot at %v: \n\n", s.now)
+	t.Logf("cwndGain        = %v\n", lim.cwndGain)
+	t.Logf("pacingGain      = %v\n", lim.pacingGain)
+	t.Logf("btlBw           = %v\n", lim.btlBwFilter.Estimate)
+	t.Logf("rtProp          = %v\n", lim.rtPropFilter.Estimate)
+	t.Logf("nextSendTime    = %v\n", lim.nextSendTime)
+	t.Logf("sent            = %v\n", lim.sent)
+	t.Logf("delivered       = %v\n", lim.delivered)
+	t.Logf("deliveredTime   = %v\n", lim.deliveredTime)
+	t.Logf("inflight        = %v\n", lim.inflight())
+	t.Logf("bdp             = %v\n", lim.computeBDP())
+	t.Logf("appLimitedUntil = %v\n", lim.appLimitedUntil)
+	t.Logf("state           = %T%v\n", lim.state, lim.state)
+
+	t.Logf("btlBw samples:\n")
+	head, tail := lim.btlBwFilter.q.Items()
+	for _, v := range head {
+		t.Logf("  %v\n", v)
+	}
+	for _, v := range tail {
+		t.Logf("  %v\n", v)
+	}
+}
 
 // TODO: work this into a proper test of... something...
 //
@@ -14,6 +46,7 @@ import (
 // we push data into a stream continuously, but it's not really measuring
 // any pass/fail criteria.
 func TestTrace(t *testing.T) {
+	var snapshots []snapshot
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	//	clock := clock.NewManual(sampleStartTime)
@@ -36,41 +69,18 @@ func TestTrace(t *testing.T) {
 			got, err := lim.StartMessage(ctx, size)
 			assert.Nil(t, err)
 			tx.Send(testPacket{
-				size: size,
-				gotResponse: func() {
-					got()
-					t.Logf("ACK'd")
-				},
+				size:        size,
+				gotResponse: got,
 			})
 			lim.whilePaused(func() {
-				// FIXME: either switch over to an artificial clock, or
-				// defer the actual printing of this data to some time when
-				// we're not actually measuring time! Otherwise it may
-				// throw off measurements by itself being the bottleneck.
-				t.Logf("Limiter snapshot: \n\n")
-				t.Logf("cwndGain        = %v\n", lim.cwndGain)
-				t.Logf("pacingGain      = %v\n", lim.pacingGain)
-				t.Logf("btlBw           = %v\n", lim.btlBwFilter.Estimate)
-				t.Logf("rtProp          = %v\n", lim.rtPropFilter.Estimate)
-				t.Logf("nextSendTime    = %v\n", lim.nextSendTime)
-				t.Logf("sent            = %v\n", lim.sent)
-				t.Logf("delivered       = %v\n", lim.delivered)
-				t.Logf("deliveredTime   = %v\n", lim.deliveredTime)
-				t.Logf("inflight        = %v\n", lim.inflight())
-				t.Logf("bdp             = %v\n", lim.computeBDP())
-				t.Logf("appLimitedUntil = %v\n", lim.appLimitedUntil)
-				t.Logf("state           = %T%v\n", lim.state, lim.state)
-
-				t.Logf("btlBw samples:\n")
-				head, tail := lim.btlBwFilter.q.Items()
-				for _, v := range head {
-					t.Logf("  %v\n", v)
-				}
-				for _, v := range tail {
-					t.Logf("  %v\n", v)
-				}
+				snapshots = append(snapshots, snapshot{
+					lim: *lim,
+					now: clock.Now(),
+				})
 			})
 		}
 	}
-
+	for _, s := range snapshots {
+		s.report(t)
+	}
 }
