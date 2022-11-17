7e79c1bdbe8484601edcab43cacd63423ec9adf7
Ian Denhardt <ian@zenhack.net>
2022-11-17 14:41:23 -0500

    bbr: add comments to exported snapshot stuff.

--- internal/bbr/snapshot.go
+++ internal/bbr/snapshot.go
@@ -6,11 +6,14 @@ import (
 	"time"
 )
 
+// A Snapshot captures the internal state of a limiter at the time it was called, along
+// with the current time. This is useful for debugging.
 type Snapshot struct {
 	lim Limiter
 	now time.Time
 }
 
+// Take a snapshot of the limiter.
 func SnapshotLimiter(l *Limiter) Snapshot {
 	var ret Snapshot
 	l.whilePaused(func() {
@@ -26,8 +29,8 @@ func SnapshotLimiter(l *Limiter) Snapshot {
 // A SnapshottingLimiter is a wrapper around Limiter which takes snapshots on each
 // operation and passes them to a callback.
 type SnapshottingLimiter struct {
-	Limiter        *Limiter
-	RecordSnapshot func(Snapshot)
+	Limiter        *Limiter       // The limiter to wrap
+	RecordSnapshot func(Snapshot) // The callback to pass snapshots to
 }
 
 func (l SnapshottingLimiter) StartMessage(ctx context.Context, size uint64) (gotResponse func(), err error) {
@@ -52,6 +55,9 @@ func (l SnapshottingLimiter) snapshot() {
 type o map[string]any
 type a []any
 
+// Returns a value that can be formatted as json using the encoding/json package, which
+// captures the data in the snapshot. The exact format of this is unspecified, as it
+// exposes implementation details of the Limiter type.
 func (s Snapshot) Json() any {
 	lim := &s.lim
 	ret := o{
@@ -69,7 +75,7 @@ func (s Snapshot) Json() any {
 		"appLimitedUntil": lim.appLimitedUntil,
 		"state": o{
 			"type":  fmt.Sprintf("%T", lim.state),
-			"value": fmt.Sprintf("%v", lim.state),
+			"value": fmt.Sprintf("%v", lim.state), // TODO: better formatting of state?
 		},
 	}
 
