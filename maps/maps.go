fcfa6a22e952acf7858b9767c5dc14a0aca60de4
Ian Denhardt <ian@zenhack.net>
2023-02-16 18:41:22 -0500

    Add a maps.Keys function

--- maps/maps.go
+++ maps/maps.go
@@ -6,6 +6,14 @@ type KV[K, V any] struct {
 	Value V
 }
 
+func Keys[K comparable, V any](m map[K]V) []K {
+	items := make([]K, 0, len(m))
+	for k, _ := range m {
+		items = append(items, k)
+	}
+	return items
+}
+
 func Items[K comparable, V any](m map[K]V) []KV[K, V] {
 	items := make([]KV[K, V], 0, len(m))
 	for k, v := range m {
