ead6a66edd352993a9b205cb046a67975344d846
Ian Denhardt <ian@zenhack.net>
2023-02-24 15:40:23 -0500

    Stubs for handling accept & provide
    I plan on adding panic("TODO: 3PH") everywhere we need to add some 3PH
    related logic; this shouldn't affect existing applications, since these
    are all conditional on us actually having a non-nil network.

--- rpc/rpc.go
+++ rpc/rpc.go
@@ -594,6 +594,19 @@ func (c *Conn) receive() error {
 			return err
 		}
 
+		sendUnimplemented := func(err error) {
+			c.er.ReportError(err)
+			c.withLocked(func(c *lockedConn) {
+				c.sendMessage(ctx, func(m rpccp.Message) error {
+					defer release()
+					if err := m.SetUnimplemented(recv); err != nil {
+						return rpcerr.Annotate(err, "send unimplemented")
+					}
+					return nil
+				}, nil)
+			})
+		}
+
 		switch recv.Which() {
 		case rpccp.Message_Which_unimplemented:
 			// no-op for now to avoid feedback loop
@@ -691,17 +704,20 @@ func (c *Conn) receive() error {
 				return err
 			}
 
+		case rpccp.Message_Which_accept:
+			if c.network == nil {
+				sendUnimplemented(errors.New("Accept from remote not supported: connection has no associated network"))
+				continue
+			}
+			panic("TODO: 3PH")
+		case rpccp.Message_Which_provide:
+			if c.network == nil {
+				sendUnimplemented(errors.New("Provide from remote not supported: connection has no associated network"))
+				continue
+			}
+			panic("TODO: 3PH")
 		default:
-			c.er.ReportError(errors.New("unknown message type " + recv.Which().String() + " from remote"))
-			c.withLocked(func(c *lockedConn) {
-				c.sendMessage(ctx, func(m rpccp.Message) error {
-					defer release()
-					if err := m.SetUnimplemented(recv); err != nil {
-						return rpcerr.Annotate(err, "send unimplemented")
-					}
-					return nil
-				}, nil)
-			})
+			sendUnimplemented(errors.New("unknown message type " + recv.Which().String() + " from remote"))
 		}
 	}
 }
