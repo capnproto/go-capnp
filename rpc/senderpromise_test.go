27b05bee208d08965412b517ebb8e07331b4fa76
Ian Denhardt <ian@zenhack.net>
2023-03-12 14:36:05 -0400

    Add a test for the handleUnimplemented logic.
    ...which hangs. Some debugging suggests that this *is* being removed
    from the table, but I need to figure out why the cap isn't actually
    released.

--- rpc/senderpromise_test.go
+++ rpc/senderpromise_test.go
@@ -68,3 +68,149 @@ func TestSenderPromiseFulfill(t *testing.T) {
 		assert.NotEqual(t, bootExportID, desc.SenderHosted)
 	}
 }
+
+// Tests that if we get an unimplemented message in response to a resolve message, we correctly
+// drop the capability.
+func TestResolveUnimplementedDrop(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+	p, r := capnp.NewLocalPromise[testcapnp.Empty]()
+
+	provider := testcapnp.EmptyProvider_ServerToClient(emptyShutdownerProvider{
+		result: p,
+	})
+
+	left, right := transport.NewPipe(1)
+	p1, p2 := rpc.NewTransport(left), rpc.NewTransport(right)
+
+	conn := rpc.NewConn(p1, &rpc.Options{
+		ErrorReporter:   testErrorReporter{tb: t},
+		BootstrapClient: capnp.Client(provider),
+	})
+	defer finishTest(t, conn, p2)
+
+	// 1. Send bootstrap.
+	{
+		msg := &rpcMessage{
+			Which:     rpccp.Message_Which_bootstrap,
+			Bootstrap: &rpcBootstrap{QuestionID: 0},
+		}
+		assert.NoError(t, sendMessage(ctx, p2, msg))
+	}
+	// 2. Receive return.
+	var bootExportID uint32
+	{
+		rmsg, release, err := recvMessage(ctx, p2)
+		assert.NoError(t, err)
+		defer release()
+		assert.Equal(t, rpccp.Message_Which_return, rmsg.Which)
+		assert.Equal(t, uint32(0), rmsg.Return.AnswerID)
+		assert.Equal(t, rpccp.Return_Which_results, rmsg.Return.Which)
+		assert.Equal(t, 1, len(rmsg.Return.Results.CapTable))
+		desc := rmsg.Return.Results.CapTable[0]
+		assert.Equal(t, rpccp.CapDescriptor_Which_senderHosted, desc.Which)
+		bootExportID = desc.SenderHosted
+	}
+	onShutdown := make(chan struct{})
+	// 3. Send finish
+	{
+		assert.NoError(t, sendMessage(ctx, p2, &rpcMessage{
+			Which: rpccp.Message_Which_finish,
+			Finish: &rpcFinish{
+				QuestionID:        0,
+				ReleaseResultCaps: false,
+			},
+		}))
+	}
+	// 4. Call getEmpty
+	{
+		assert.NoError(t, sendMessage(ctx, p2, &rpcMessage{
+			Which: rpccp.Message_Which_call,
+			Call: &rpcCall{
+				QuestionID: 1,
+				Target: rpcMessageTarget{
+					Which:       rpccp.MessageTarget_Which_importedCap,
+					ImportedCap: bootExportID,
+				},
+				InterfaceID: testcapnp.EmptyProvider_TypeID,
+				MethodID:    0,
+				Params:      rpcPayload{},
+			},
+		}))
+	}
+	// 5. Receive return.
+	var emptyExportID uint32
+	{
+		rmsg, release, err := recvMessage(ctx, p2)
+		assert.NoError(t, err)
+		defer release()
+		assert.Equal(t, uint32(1), rmsg.Return.AnswerID)
+		assert.Equal(t, rpccp.Return_Which_results, rmsg.Return.Which)
+		assert.Nil(t, rmsg.Return.Exception)
+		assert.Equal(t, 1, len(rmsg.Return.Results.CapTable))
+		desc := rmsg.Return.Results.CapTable[0]
+		assert.Equal(t, rpccp.CapDescriptor_Which_senderPromise, desc.Which)
+		emptyExportID = desc.SenderPromise
+	}
+	// 7. Fulfill promise
+	{
+		pp := testcapnp.Empty_ServerToClient(emptyShutdowner{
+			onShutdown: onShutdown,
+		})
+		defer pp.Release()
+		r.Fulfill(pp)
+	}
+	// 8. Receive resolve, send unimplemented
+	{
+		rmsg, release, err := recvMessage(ctx, p2)
+		assert.NoError(t, err)
+		defer release()
+		assert.Equal(t, rpccp.Message_Which_resolve, rmsg.Which)
+		assert.Equal(t, emptyExportID, rmsg.Resolve.PromiseID)
+		assert.Equal(t, rpccp.Resolve_Which_cap, rmsg.Resolve.Which)
+		desc := rmsg.Resolve.Cap
+		assert.Equal(t, rpccp.CapDescriptor_Which_senderHosted, desc.Which)
+		assert.NoError(t, sendMessage(ctx, p2, &rpcMessage{
+			Which:         rpccp.Message_Which_unimplemented,
+			Unimplemented: rmsg,
+		}))
+	}
+	// 9. Drop the promise on our side. Otherwise it will stay alive because of
+	// the bootstrap interface:
+	{
+		p.Release()
+	}
+	// 6. Send finish
+	{
+		assert.NoError(t, sendMessage(ctx, p2, &rpcMessage{
+			Which: rpccp.Message_Which_finish,
+			Finish: &rpcFinish{
+				QuestionID:        1,
+				ReleaseResultCaps: true,
+			},
+		}))
+	}
+	<-onShutdown // Will hang unless the capability is dropped
+}
+
+type emptyShutdownerProvider struct {
+	result testcapnp.Empty
+}
+
+func (e emptyShutdownerProvider) GetEmpty(ctx context.Context, p testcapnp.EmptyProvider_getEmpty) error {
+	results, err := p.AllocResults()
+	if err != nil {
+		return err
+	}
+	results.SetEmpty(e.result)
+	return nil
+}
+
+type emptyShutdowner struct {
+	onShutdown chan<- struct{} // closed on shutdown
+}
+
+func (s emptyShutdowner) Shutdown() {
+	close(s.onShutdown)
+}
