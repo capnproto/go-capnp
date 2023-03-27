5006b5c3d8ac4462b7c761a403ec98ad88fbf25f
Ian Denhardt <ian@zenhack.net>
2023-03-27 17:27:36 -0400

    Add Lock() method for Mutex.
    ...for more complex synchronization patterns than can be accommodated by
    With, which unfortunately go-capnp has a few of.

--- sync/mutex/mutex.go
+++ sync/mutex/mutex.go
@@ -24,6 +24,17 @@ func (m *Mutex[T]) With(f func(*T)) {
 	f(&m.val)
 }
 
+// Lock acquires the mutex and returns a reference to the value. Call Unlock()
+// on the return value to release the lock.
+//
+// Where possible, you should prefer using Mutex.With and similar functions. If
+// those are insufficient to handle your use case consider whether your locking
+// scheme is too complicated before going ahead and using this.
+func (m *Mutex[T]) Lock() *Locked[T] {
+	m.mu.Lock()
+	return &Locked[T]{mu: m}
+}
+
 // With1(m, ...) is like m.With(...), except that the callback returns
 // a value.
 func With1[T, A any](m *Mutex[T], f func(*T) A) A {
@@ -44,4 +55,30 @@ func With2[T, A, B any](m *Mutex[T], f func(*T) (A, B)) (A, B) {
 		a, b = f(t)
 	})
 	return a, b
+}
+
+// A Locked[T] is a reference to a value of type T which is guarded by a Mutex,
+// which the caller has acquired.
+type Locked[T any] struct {
+	mu       *Mutex[T]
+	unlocked bool
+}
+
+// Value returns a reference to the protected value. It must not be used after
+// calling Unlock.
+func (l *Locked[T]) Value() *T {
+	if l.unlocked {
+		panic("Called Locked.Value after Unlock.")
+	}
+	return &l.mu.val
+}
+
+// Unlock releases the mutex. Any references to the value obtained via Value
+// must not be used after this is called.
+func (l *Locked[T]) Unlock() {
+	if l.unlocked {
+		panic("Called Locked.Unlock twice.")
+	}
+	l.unlocked = true
+	l.mu.mu.Unlock()
 }
